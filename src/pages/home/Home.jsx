c94307d85124cfcc942c1303cce320f565a39d45
jihadkhan934 <jihadkhan934@gmail.com>
2023-10-06 12:10:26 +0600

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-center text-3xl'>
+            home
+        </div>
+    );
+};
+
+export default Home;
