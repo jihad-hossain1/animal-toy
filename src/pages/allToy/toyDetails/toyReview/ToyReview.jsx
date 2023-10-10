15f6e06e500eca9254d4e4174ce618016dd1d373
jihad-hossain1 <jihadkhan934@gmail.com>
2023-10-10 18:21:07 +0600

    some change

--- src/pages/allToy/toyDetails/toyReview/ToyReview.jsx
+++ src/pages/allToy/toyDetails/toyReview/ToyReview.jsx
@@ -1,36 +1,46 @@
 import { TabPanel } from "@material-tailwind/react";
-import React from "react";
+import { useReview } from "../../../../hooks/useReview";
+import { useContext } from "react";
+import { AuthContext } from "../../../../authentication/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
 
 const ToyReview = ({ singleToy }) => {
-    const commentTime = new Date().toLocaleDateString()
-    const commentDate = new Date().toLocaleTimeString()
-    const commmentTime = {
-        commentDate,commentTime
+  const {user} = useContext(AuthContext)
+  // to get local time 
+    const reviewDate = new Date().toLocaleDateString()
+    const reviewTime = new Date().toLocaleTimeString()
+    const reviewTimes = {
+        reviewDate,reviewTime
     }
-    const [comments,refetch,loading] = useComment()
-  const handleComment = async (e) => {
+    // custom hook
+    const [reviews,refetch,loading,isError] = useReview()
+    // rating picker 
+    const [value, setValue] = useState(0);
+    const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
+    // handle submited review section
+  const handleReview = async (e) => {
     e.preventDefault();
     if (!user) {
       return toast.error("Please Login first!");
     } else {
       const form = e.target;
-      const commentUserName = form.commentUserName.value;
-      const commentUserComment = form.commentUserComment.value;
+      const reviewUserName = form.reviewUserName.value;
+      const reviewUserreview = form.reviewUserreview.value;
     //   console.log(value)
       const info = {
-          comment: commentUserComment,
-          commentUser: commentUserName,
+          review: reviewUserreview,
+          reviewUser: reviewUserName,
           userEmail: user?.email,
           userNaem: user?.displayName,
           userPhoto: user?.photoURL,
-          commentCount: parseFloat(value),
-          commentID: course?._id,
-          timeDate: commmentTime 
+          reviewCount: parseFloat(value),
+          reviewID: course?._id,
+          timeDate: reviewTimes 
       };
 // console.log(info)
       try {
         const res = await fetch(
-          `${import.meta.env.VITE_BASE_URL}/comments`,
+          `${import.meta.env.VITE_BASE_URL}/reviews`,
           {
             method: "POST",
             headers: {
@@ -42,7 +52,7 @@ const ToyReview = ({ singleToy }) => {
         const data = await res.json();
           console.log(data)
         if (data) {
-          toast.success(`comments successfull`);
+          toast.success(`reviews successfull`);
           refetch()
           form.reset()
         }
@@ -55,7 +65,47 @@ const ToyReview = ({ singleToy }) => {
 
   return (
     <TabPanel value={"review"} >
-      <div className="min-h-[200px]"></div>
+      <Toaster />
+      <div className="min-h-[200px]">
+        {/* comment or review section  */}
+        <hr className='py-4' />
+        <div className=''>
+          <h4 className='text-blue-gray-600 font-semibold px-2'>
+            Leave a comments..
+          </h4>
+          <form
+            onSubmit={handleReview}
+            action=''
+            className='max-w-[450px] px-2 py-2'
+          >
+           <span>
+           <Rate tooltips={desc} onChange={setValue} allowHalf className="mb-2" defaultValue={course?.instructorRating} />
+            {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
+           </span>
+            <div className='mb-3'>
+              <Input
+                required
+                name='commentUserName'
+                label='Your Name'
+                color='teal'
+              ></Input>
+            </div>
+            <Textarea
+              required
+              name='commentUserComment'
+              label='Leave a comments'
+              color='teal'
+            ></Textarea>
+            <Button label='' color='teal' type='submit'>
+              submit
+            </Button>
+          </form>
+        </div>
+        <hr className='py-4' />
+        <div className={`${comment.length > 0 ? 'border border-cyan-50 p-4 rounded text-blue-gray-700 grid gap-3' : 'text-blue-gray-600'}`}>
+            {comment.length > 0 ? comment.map(item=><SingleComment item={item} key={item._id} refetch={refetch}></SingleComment>) : 'Ops No comments here..' }
+        </div>
+      </div>
     </TabPanel>
   );
 };
