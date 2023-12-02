d4e2b8b6cb5007bfb66afe69027b08daf1adaad3
jihad-hossain1 <jihadkhan934@gmail.com>
2023-12-02 06:35:35 +0600

    some change

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -20,95 +20,90 @@ import Carts from "../pages/dashboard/user/userProfile/Carts";
 import UpdateToy from "../pages/dashboard/seller/updateToy/UpdateToy";
 import Checkout from "../pages/dashboard/user/checkout/Checkout";
 
-
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main></Main>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-            {
-                path: '/alltoys',
-                element: <AllToy></AllToy>
-            }
-           ,
-            {
-                path: '/singletoy/:id',
-                element: <ToyDetails></ToyDetails>,
-                loader: async({ params }) => await fetch(`${import.meta.env.VITE_BASE_URL}/toys/${params.id}`)
-            }
-           ,
-            {
-                path: '/whitelist',
-                element: <WhiteList></WhiteList>
-            }
-           ,
-            {
-                path: '/tranding',
-                element: <Tranding></Tranding>
-            }
-           ,
-            {
-                path: '/contact',
-                element: <Contact></Contact>
-            }
-           ,
-            {
-                path: '/checkout',
-                element: <Checkout></Checkout>
-            }
-           ,
-            {
-                path: '/blogs',
-                element: <Blogs></Blogs>
-            },
-            {
-                path: '/userDashboard',
-                element: <UserDashboardHome />,
-            }
-        ]
-    },
-    {
-        path: '/dashboardAdminOnly',
-        element: <AdminDashboard />,
-        children: [ 
-            {
-                path: '/dashboardAdminOnly',
-                element: <AdminDashboardHome />,
-            }
-        ]
-    },
-    {
-        path: '/dashboardSellerOnly',
-        element: <SellerDashboard />,
-        children: [ 
-            {
-                path: '/dashboardSellerOnly',
-                element: <SellerDashboardHome />,
-            },
-            {
-                path: '/dashboardSellerOnly/addToy',
-                element: <AddToy />,
-            },
-            {
-                path: '/dashboardSellerOnly/manageToy',
-                element: <ManageToy />
-            }
-            ,
-            {
-                path: '/dashboardSellerOnly/carts',
-                element: <Carts />
-            }
-            
-        ]
-    },
-    
-    {
-        path: '/login',
-        element: <Login />
-    }
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/alltoys",
+        element: <AllToy></AllToy>,
+      },
+      {
+        path: "/singletoy/:id",
+        element: <ToyDetails></ToyDetails>,
+        loader: async ({ params }) =>
+          await fetch(`${import.meta.env.VITE_BASE_URL}/toys/${params.id}`),
+      },
+      {
+        path: "/whitelist",
+        element: <WhiteList></WhiteList>,
+      },
+      {
+        path: "/tranding",
+        element: <Tranding></Tranding>,
+      },
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout></Checkout>,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs></Blogs>,
+      },
+      {
+        path: "/userDashboard",
+        element: <UserDashboardHome />,
+      },
+      {
+        path: "/abc",
+        element: <div>abc</div>,
+      },
+    ],
+  },
+  {
+    path: "/dashboardAdminOnly",
+    element: <AdminDashboard />,
+    children: [
+      {
+        path: "/dashboardAdminOnly",
+        element: <AdminDashboardHome />,
+      },
+    ],
+  },
+  {
+    path: "/dashboardSellerOnly",
+    element: <SellerDashboard />,
+    children: [
+      {
+        path: "/dashboardSellerOnly",
+        element: <SellerDashboardHome />,
+      },
+      {
+        path: "/dashboardSellerOnly/addToy",
+        element: <AddToy />,
+      },
+      {
+        path: "/dashboardSellerOnly/manageToy",
+        element: <ManageToy />,
+      },
+      {
+        path: "/dashboardSellerOnly/carts",
+        element: <Carts />,
+      },
+    ],
+  },
 
-])
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
